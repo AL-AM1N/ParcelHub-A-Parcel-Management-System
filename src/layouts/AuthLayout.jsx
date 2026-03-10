cc873c9641b8cda58366f68e3f286d865c804b01
MD. AL-AMIN <amin57.cse@gmail.com>
2026-03-10 22:45:48 +0600

    AuthLayout created and react-hook-form installed

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Outlet } from "react-router";
+import authImage from "../assets/authImage.png";
+import logo from "../assets/logo.png";
+
+const AuthLayout = () => {
+  return (
+    <div className="bg-base-200 max-w-375 mx-auto">
+      <div>
+        <Link to="/">
+          <img className="w-50 h-16" src={logo} alt="ParcelHub logo" />
+        </Link>
+      </div>
+      <div className="hero-content mx-auto flex-col lg:flex-row-reverse">
+        <div className="flex-1 place-items-center">
+          <img src={authImage} className="max-w-sm rounded-lg shadow-2xl" />
+        </div>
+        <div className="flex-1 place-items-center ">
+          <Outlet></Outlet>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,29 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component:RootLayout,
-    children:[
+    Component: RootLayout,
+    children: [
       {
-        index:true,
-        path:'/',
-        Component:Home
-      }
-    ]
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
+  }
 ]);
