f18db32e263cbd004247cc7ac972d4af38c001c1
MD. AL-AMIN <amin57.cse@gmail.com>
2026-03-11 22:38:07 +0600

    login form created and handle it using react-hook-form

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,19 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <fieldset className="fieldset">
-        <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
-        <div>
-          <a className="link link-hover">Forgot password?</a>
-        </div>
-        <button className="btn btn-neutral mt-4">Login</button>
-      </fieldset>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email")}
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true, 
+              minLength:6
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
   );
 };
 
