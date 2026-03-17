a2a3c075297d952ac4573716be71c0baf7c5f19d
MD. AL-AMIN <amin57.cse@gmail.com>
2026-03-18 04:12:47 +0600

    load specific parcel info and show the parcel cost in the ui

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,11 +1,32 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const {parcelId} = useParams();
+  //console.log(parcelId)
+  const axiosSecure = useAxiosSecure();
 
   const [error, setError] = useState('');
+
+  const {isPending, data: parcelInfo = {}} = useQuery({
+    queryKey:['parcels', parcelId],
+    queryFn: async() => {
+        const res = await axiosSecure.get(`/parcels/${parcelId}`);
+        return res.data;
+    }
+  })
+
+  if(isPending){
+    return <span className="loading loading-bars loading-xl"></span>;
+  }
+
+  console.log(parcelInfo)
+  const amount = parcelInfo.cost;
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -41,7 +62,7 @@ const PaymentForm = () => {
       <CardElement className="p-2 border rouded">
       </CardElement>
         <button className="btn btn-primary w-full text-black" type="submit" disabled={!stripe}>
-          Pay for parcel pickup
+          Pay ${amount}
         </button>
         {
             error && <p className="text-red-500">{error}</p>
